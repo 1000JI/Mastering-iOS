fd690384e49012f317db003791f2dc2186b6e1b9
1000JI <chjh1992@gmail.com>
2021-02-05 12:56:31 +0900

    custom presentation study

--- old-xcode9+/AnimationTransition/Transition/Transition/Customizing/CustomPresentation/CustomPresentationViewController.swift
+++ old-xcode9+/AnimationTransition/Transition/Transition/Customizing/CustomPresentation/CustomPresentationViewController.swift
@@ -23,29 +23,20 @@
 import UIKit
 
 class CustomPresentationViewController: UIViewController {
-   
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      
-   }
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      
-   }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.modalPresentationStyle = .custom
+        segue.destination.transitioningDelegate = self
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+extension CustomPresentationViewController: UIViewControllerTransitioningDelegate {
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        return SimplePresentationController(presentedViewController: presented, presenting: presenting)
+    }
+}
--- old-xcode9+/AnimationTransition/Transition/Transition/ViewTransition/ViewTransitionViewController.swift
+++ old-xcode9+/AnimationTransition/Transition/Transition/ViewTransition/ViewTransitionViewController.swift
@@ -23,18 +23,36 @@
 import UIKit
 
 class ViewTransitionViewController: UIViewController {
-   
-   @IBOutlet weak var containerView: UIView!
-   @IBOutlet weak var redView: UIView!
-   @IBOutlet weak var blueView: UIView!
-   
-   @IBAction func startTransition(_ sender: Any) {
-
-   }
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      
-      
-   }
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    
+    @IBAction func startTransition(_ sender: Any) {
+//        UIView.transition(with: containerView,
+//                          duration: 1,
+//                          options: [.transitionCurlUp],
+//                          animations: {
+//                            self.redView.isHidden.toggle()
+//                            self.blueView.isHidden.toggle()
+//                          },
+//                          completion: nil)
+        
+        UIView.transition(from: redView, // 제거 할 View
+                          to: blueView, // 추가 할 View
+                          duration: 1,
+                          options: [.transitionFlipFromLeft, .showHideTransitionViews],
+                          completion: { finished in
+                            UIView.transition(from: self.blueView,
+                                              to: self.redView,
+                                              duration: 1,
+                                              options: [.transitionFlipFromRight, .showHideTransitionViews],
+                                              completion: nil)
+                          })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
 }
